d68f276f86fd4c3f2ab9551b870d16ff9a002b9e
Oksana <ereminaoksana82@yandex.ru>
2022-09-01 16:02:40 +0400

    HW2_Task10

---
+++ Hw2_task10/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+Console.WriteLine ("Введите число: ");
+string? line = Console.ReadLine();
+if (line != null) {
+    char[] array = line.ToCharArray();
+    Console.WriteLine(array[1]);
+}
+
